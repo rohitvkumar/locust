70d302bd5fca8e8af09cb425ab2949e115594de9
Rohit Valsakumar <rohit.clique.intel@gmail.com>
2018-11-20 22:06:42 -0800

    Usqe1 bodyIds

--- locustfile.py
+++ locustfile.py
@@ -2,26 +2,26 @@ from locust import HttpLocust, TaskSet, task
 
 class WebsiteTasks(TaskSet):
     @task
-    def req_050001F359206076(self):
-        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001F359206076"})
+    def req_1(self):
+        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:0500012C0067F4EC"})
 
     @task
     def req_050001F2E1AB12E1(self):
-        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001F2E1AB12E1"})
+        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:05000137B41D9EC6"})
 
     @task
     def req_050001D58A983AE6(self):
-        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001D58A983AE6"})
+        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001F8CAA3C487"})
 
     @task
     def req_050001724C8EACD9(self):
-        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001724C8EACD9"})
+        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:05000163AA20FA3D"})
     
     @task
     def req_050001552F5A5E8E(self):
-        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001552F5A5E8E"})
+        self.client.post("/anonAccountInfoGet", json={"type": "anonAccountInfoGet", "bodyId":"int:050001A6E1DE4438"})
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    min_wait = 100
-    max_wait = 200
+    min_wait = 10
+    max_wait = 20
